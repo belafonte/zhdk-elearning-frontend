37aa4d177c9036e3d7c67efda5d5731ae5526770
Jan Pistor <regproxy0@gmail.com>
2022-10-25 12:27:22 +0200

    fix: menu not closing

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -14,8 +14,17 @@
 
 	function openMenu() {
 		if (body !== null) {
-			menuOpened ? body.classList.remove("overflow-hidden") : body.classList.add("overflow-hidden");
-			menuOpened = !menuOpened;
+			body.classList.add("overflow-hidden");
+			menuOpened = true;
+		} else {
+			console.warn("body not available");
+		}
+	}
+
+	function closeMenu() {
+		if (body !== null) {
+			body.classList.remove("overflow-hidden");
+			menuOpened = false;
 		} else {
 			console.warn("body not available");
 		}
@@ -30,10 +39,9 @@
 	<header class="sticky top-0 mb-[84px] bg-white z-50 w-full" class:h-screen={menuOpened}>
 		<nav class="text-36 sm:text-68 h-full flex flex-col">
 			<div class=" flex border-b border-light-gray p-15 pb-5">
-				<a href="/" on:click={() => (menuOpened = false)}>E</a>
-				<a href="/" on:click={() => (menuOpened = false)} class="ml-auto font-sans flex">Learning</a
-				>
-				<button on:click={() => openMenu()}
+				<a href="/" on:click={() => closeMenu()}>E</a>
+				<a href="/" on:click={() => closeMenu()} class="ml-auto font-sans flex">Learning</a>
+				<button on:click={() => (!menuOpened ? openMenu() : closeMenu())}
 					><img
 						alt="Open Menu Button"
 						class="w-32 sm:w-60 pb-10 ml-15 transition-transform"
@@ -46,15 +54,15 @@
 				<div
 					id="menu"
 					class="w-full h-full grid grid-cols-1 lg:grid-cols-2"
-					on:click={() => (menuOpened = false)}
+					on:click={() => closeMenu()}
 				>
 					<div class="flex flex-col">
 						<a>Leitfragen</a>
-						<a>Events</a>
+						<a href="/event/overview">Events</a>
 						<a href="/knowledge/overview">Knowledge in use</a>
 						<a href="/kosmos/overview">Kosmos</a>
 						<a href="/community/overview">Community</a>
-						<a>Glossar</a>
+						<a href="/glossary">Glossar</a>
 						<a class="mt-auto mb-10 text-12 sm:!text-14 !text-black uppercase ">Journal</a>
 					</div>
 					<div class="flex flex-col border-t sm:border-l ">

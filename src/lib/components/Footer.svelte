c26ad3e5de23b939c9ef69ef5fc55a3bd1339719
Jan Pistor <regproxy0@gmail.com>
2022-10-25 18:05:08 +0200

    fix: offer link in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,7 +6,7 @@
 	<div class="mt-32 sm:mt-84 border-t flex flex-col bg-white ">
 		<h1 class="text-24 sm:text-50 p-50 self-center">Recherche zu digitalem Lehren und Lernen</h1>
 		<div class="footer-links px-15 border-t grid grid-cols-2 sm:grid-cols-4">
-			<div class="border-l"><a href="/angebot">Angebot</a></div>
+			<div class="border-l"><a href="/offer">Angebot</a></div>
 			<div>
 				<a href="/information">Information</a>
 			</div>

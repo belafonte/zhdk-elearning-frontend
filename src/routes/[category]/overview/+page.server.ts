83fcb3ab84a4c5839c9fc7df8ce8e47a8c957ef3
Jan Pistor <pistor.jan@gmail.com>
2023-04-12 10:35:31 +0200

    refactor: using auto adapter with prerender auto

--- src/routes/[category]/overview/+page.server.ts
+++ src/routes/[category]/overview/+page.server.ts
@@ -4,31 +4,31 @@ import type { GetTileDataQuery } from "$graphql/types";
 import { GET_TILE_DATA } from "$graphql/queries";
 
 export const load: PageServerLoad = async ({ params }) => {
-  const category = params.category;
-  const filterCategory = category.replace(/^./, category.charAt(0).toUpperCase());
-  const filterDate =
-    category === "event" ? { $gte: new Date().toISOString().substring(0, 10) } : null;
-  const sort = category === "event" ? { "event.fromDate": true } : { _created: true };
+	const category = params.category;
+	const filterCategory = category.replace(/^./, category.charAt(0).toUpperCase());
+	const filterDate =
+		category === "event" ? { $gte: new Date().toISOString().substring(0, 10) } : null;
+	const sort = category === "event" ? { "event.fromDate": true } : { _created: true };
 
-  const posts = await URQLClient.query<GetTileDataQuery>(GET_TILE_DATA, {
-    limit: null,
-    filter: { category: filterCategory, "event.fromDate": filterDate },
-    sort: sort
-  })
-    .toPromise()
-    .then((res) => res.data?.contentModel);
+	const posts = await URQLClient.query<GetTileDataQuery>(GET_TILE_DATA, {
+		limit: null,
+		filter: { category: filterCategory, "event.fromDate": filterDate },
+		sort: sort
+	})
+		.toPromise()
+		.then((res) => res.data?.contentModel);
 
-  let cols: { mobile: number; tablet: number; desktop: number };
+	let cols: { mobile: number; tablet: number; desktop: number };
 
-  switch (params.category) {
-    case "insights":
-    case "community":
-      cols = { mobile: 1, tablet: 2, desktop: 2 };
-      break;
-    default:
-      cols = { mobile: 2, tablet: 3, desktop: 4 };
-      break;
-  }
+	switch (params.category) {
+		case "insights":
+		case "community":
+			cols = { mobile: 1, tablet: 2, desktop: 2 };
+			break;
+		default:
+			cols = { mobile: 2, tablet: 3, desktop: 4 };
+			break;
+	}
 
-  return { cols: cols, highlightColor: "#EEEEEE", category: category, posts };
+	return { cols: cols, highlightColor: "#EEEEEE", category: category, posts };
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from "@sveltejs/adapter-auto";
-import { adapter as adapterStatic } from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -16,15 +16,7 @@ const config = {
 		alias: {
 			$graphql: "./src/graphql/"
 		},
-		adapter: adapterStatic({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: "build",
-			assets: "build",
-			fallback: null,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		csp: {
 			mode: "hash"
 		}

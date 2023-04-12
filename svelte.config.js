a7145a66309b0e4c1bbdbc38377eed1035e90d11
Jan Pistor <pistor.jan@gmail.com>
2023-04-12 10:15:25 +0200

    refactor: use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+import { adapter as adapterStatic } from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,7 +16,15 @@ const config = {
 		alias: {
 			$graphql: "./src/graphql/"
 		},
-		adapter: adapter(),
+		adapter: adapterStatic({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: "build",
+			assets: "build",
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
 		csp: {
 			mode: "hash"
 		}

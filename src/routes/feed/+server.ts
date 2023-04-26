fdafbc376c2ed0330456c5031a36b88884022d68
Jan Pistor <pistor.jan@gmail.com>
2023-04-26 12:14:14 +0200

    refactor: remove rss in favor of pure xml

--- src/routes/feed/+server.ts
+++ src/routes/feed/+server.ts
@@ -1,6 +1,7 @@
 import { error } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
-import rss from "rss";
+// import rss from "rss";
+import sanitizeHtml from "sanitize-html";
 import URQLClient from "$graphql/urqlClient";
 import type { GetRssDataQuery } from "$graphql/types";
 import { GET_RSS_DATA } from "$graphql/queries";
@@ -18,43 +19,88 @@ export const GET = (async () => {
 	if (!posts) {
 		throw error(500, "No Items in Feed");
 	}
+
+	// Define the RSS feed as a string
+	// const rssItems = posts.map((post) => {
+	// 	const date = post?._created ? new Date(post?._created * 1000) : "";
+	//
+	// 	return {
+	// 		title: post?.title || "",
+	// 		description: post?.subhead || "",
+	// 		url: `https://elearning.zhdk.ch/${post?.category}/${post?.slug}` || "",
+	// 		date: date,
+	// 		enclosure: {
+	// 			url: PUBLIC_ASSETS + post?.title_image.path,
+	// 			type: post?.title_image.mime,
+	// 			size: post?.title_image.size
+	// 		}
+	// 	};
+	// });
+	//
+	// const feed = new rss({
+	// 	title: "ZHdK E-Learning RSS Feed",
+	// 	description: "RSS Feed showing the last ten modified posts",
+	// 	feed_url: "https://elearning.zhdk.ch/feed",
+	// 	site_url: "https://elearning.zhdk.ch",
+	// 	image_url: "https://elearning.zhdk.ch/logo.png",
+	// 	managingEditor: "manuel.weibel@zhdk.ch (Manuel Weibel)",
+	// 	webMaster: "hello@bureau314.ch (Jan Pistor)",
+	// 	language: "de-CH",
+	// 	pubDate: new Date(),
+	// 	ttl: 60
+	// });
+	//
+	// rssItems.forEach((rssItem) => {
+	// 	feed.item(rssItem);
+	// });
+
 	const rssItems = posts.map((post) => {
 		const date = post?._created ? new Date(post?._created * 1000) : "";
 
-		return {
-			title: post?.title || "",
-			description: post?.subhead || "",
-			url: `https://elearning.zhdk.ch/${post?.category}/${post?.slug}` || "",
-			date: date,
-			enclosure: {
-				url: PUBLIC_ASSETS + post?.title_image.path,
-				type: post?.title_image.mime,
-				size: post?.title_image.size
-			}
-		};
+		return `
+		<item>
+			<title>${sanitizeHtml(post?.title, { allowedTags: [], allowedAttributes: [] }) || ""}</title>
+			<description>${
+				sanitizeHtml(post?.subhead, { allowedTags: [], allowedAttributes: [] }) || ""
+			}</description>
+			<link>${
+				"https://elearning.zhdk.ch/" +
+					post?.category.toLowerCase() +
+					"/" +
+					sanitizeHtml(post?.slug, { allowedTags: [], allowedAttributes: [] }) || ""
+			}</link>
+			<pubDate>${date}</pubDate>
+			<enclosure url="${PUBLIC_ASSETS + post?.title_image.path}" type="${post?.title_image.mime}" size="${
+			post?.title_image.size
+		}"></enclosure>
+		</item>`;
 	});
+	const rssFeed = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+  <channel>
+    <title>ZHdK E-Learning RSS Feed</title>
+    <description>RSS Feed showing the last ten modified posts</description>
+    <link>https://elearning.zhdk.ch/feed</link>
+    <language>de-ch</language>
+		<managingEditor>manuel.weibel@zhdk.ch (Manuel Weibel)</managingEditor>
+		<webMaster>hello@bureau314.ch (Jan Pistor)</webMaster>
+<image>https://elearning.zhdk.ch/logo.png</image>
+    <pubDate>${new Date().toUTCString()}</pubDate>
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+    <image>
+      <url>https://example.com/images/logo.png</url>
+      <title>My RSS Feed</title>
+      <link>https://example.com</link>
+    </image>
+			${rssItems}
+  </channel>
+</rss>`;
 
-	const feed = new rss({
-		title: "ZHdK E-Learning RSS Feed",
-		description: "RSS Feed showing the last ten modified posts",
-		feed_url: "https://elearning.zhdk.ch/feed",
-		site_url: "https://elearning.zhdk.ch",
-		image_url: "https://elearning.zhdk.ch/logo.png",
-		managingEditor: "manuel.weibel@zhdk.ch (Manuel Weibel)",
-		webMaster: "hello@bureau314.ch (Jan Pistor)",
-		language: "de-CH",
-		pubDate: new Date(),
-		ttl: 60
-	});
-
-	rssItems.forEach((rssItem) => {
-		feed.item(rssItem);
-	});
-
-	const xml = feed.xml({ indent: true });
+	const xml = rssFeed;
 	const headers = {
 		"Cache-Control": "max-age=0, s-maxage=3600",
 		"Content-Type": "application/xml"
 	};
+
 	return new Response(xml, { headers: headers });
 }) satisfies RequestHandler;

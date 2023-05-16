be40e395ab82792defbb1f8c02595457bb92fde6
Jan Pistor <pistor.jan@gmail.com>
2023-05-16 12:16:42 +0200

    fix: titles line clamp

--- src/lib/components/shared/Tile.svelte
+++ src/lib/components/shared/Tile.svelte
@@ -35,7 +35,7 @@
 		style="background-color: {color && color.colors ? color.colors[0] : '#EEEEEE'}"
 	>
 		<h1
-			class="line-clamp-6 overflow-hidden text-ellipsis pb-5 sm:line-clamp-5"
+			class="mb-5 line-clamp-6 overflow-hidden text-ellipsis sm:line-clamp-5"
 			class:title-large={category === "Community"}
 			class:title-small={category === "Experience" || category === "Cosmos"}
 		>
